a2c13cd80cfb032bec7304cc1d7c303a01931e98
werrons <bakytsanzirov13@gmail.com>
2023-10-30 09:55:58 +0600

    nepolnyi

---
+++ script.js
@@ -0,0 +1,105 @@
+// function myFunction() {
+//     let a = 44
+//     let b = 23
+//     return a + b
+// }
+
+
+// let str = 'hello world'
+//
+// function tatyana() {
+//     let res = str.slice(0, 5).toUpperCase()
+//     return res
+// }
+//
+// console.log(tatyana())
+// console.log(myFunction())
+
+
+// function myFunction() {
+//     let a = 'Admin'
+//     if (a.toLowerCase() === 'admin') {
+//         return'Welcome admin'
+//     } else {
+//         return 'Welcome user'
+//     }
+// }
+
+// console.log(myFunction())
+
+
+// function myFunction() {
+//     let a = 'Aidin'
+//     if (a.toLowerCase() === 'admin') {
+//         return'Welcome admin'
+//     } else {
+//         return `Welcome ${a}`
+//     }
+// }
+// console.log(myFunction())
+
+
+// function myFunction(name) {
+//     if (name.toLowerCase() === 'admin') {
+//         return'Welcome admin'
+//     } else {
+//         return `Welcome ${name}`
+//     }
+// }
+// console.log(myFunction(`aidin`))
+// console.log(myFunction(`bolot`))
+// console.log(myFunction(`elba`))
+// console.log(myFunction(`medet`))
+
+
+// function myFunction(age) {
+//     if (age >= '18') {
+//         return'Welcome'
+//     } else {
+//         return `access denied`
+//     }
+// }
+// console.log(myFunction(`12`))
+
+// || это 'или'
+// && это 'и'
+
+//                                       H O M E      W O R K
+
+// function myFunction(user) {
+//     if (user ==='aidin') {
+//         return`user admin true welcome ${user}`
+//     } else {
+//         return `false not correct user`
+//     }
+// }
+// console.log(myFunction(`aidin`))
+
+
+// function myFunction(day) {
+//     if (day >= 1 && day <= 28 ) {
+//         return`today is ${day} of february`
+//     } else {
+//         return `on february only 28 day, your day is ${day - 28}  of another month`
+//     }
+// }
+// console.log(myFunction(30))
+
+// function myFunction(user, age) {
+//     if ( age >= 1 && age <= 28 ) {
+//         return `${user} ${age} old, welcome`
+//     } else {
+//         return `${user} ${age} old, rejected`
+//     }
+// }
+// console.log(myFunction('a', 2)
+
+
+function myFunction(user) {
+    if (user.maxLength = 3) {
+        return`today is ${user} of february`
+    } else {
+        return `on february only 28 day, your day is 3 min
+    }
+}
+c
